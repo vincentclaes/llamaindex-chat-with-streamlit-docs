579e8cfa40a94731f2c1b36c7ee46e19f6049f6e
Vincent Claes <vincent.v.claes@gmail.com>
2023-10-16 19:14:39 +0200

    load data to weaviate

---
+++ data_embedding.py
@@ -0,0 +1,84 @@
+import os
+import weaviate
+from weaviate import EmbeddedOptions
+from llama_index import download_loader
+from llama_index.vector_stores import WeaviateVectorStore
+from llama_index import VectorStoreIndex, StorageContext
+from pathlib import Path
+import argparse
+
+
+def get_pdf_files(base_path, loader):
+    """
+    Get paths to all PDF files in a directory and its subdirectories.
+
+    Parameters:
+    - base_path (str): The path to the starting directory.
+
+    Returns:
+    - list of str: A list of paths to all PDF files found.
+    """
+    pdf_paths = []
+
+    # Check if the base path exists and is a directory
+    if not os.path.exists(base_path):
+        raise FileNotFoundError(f"The specified base path does not exist: {base_path}")
+    if not os.path.isdir(base_path):
+        raise NotADirectoryError(
+            f"The specified base_path is not a directory: {base_path}"
+        )
+
+    # Loop through all directories and files starting from the base path
+    for root, dirs, files in os.walk(base_path):
+        for filename in files:
+            # If a file has a .pdf extension, add its path to the list
+            if filename.endswith(".pdf"):
+                pdf_file = loader.load_data(file=Path(root, filename))
+                pdf_paths.extend(pdf_file)
+
+    return pdf_paths
+
+
+def main(data_dir, index_name="Main", query=None):
+    PDFReader = download_loader("PDFReader")
+    loader = PDFReader()
+
+    documents = get_pdf_files(data_dir, loader)
+
+    client = weaviate.Client(
+        embedded_options=EmbeddedOptions(),
+        additional_headers={"X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"]},
+    )
+
+    # construct vector store
+    vector_store = WeaviateVectorStore(
+        weaviate_client=client, index_name=index_name, text_key="content"
+    )
+
+    # setting up the storage for the embeddings
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    # set up the index
+    index = VectorStoreIndex(documents, storage_context=storage_context)
+    if query:
+        print("Running a test query to make sure the index is working...")
+        query_engine = index.as_query_engine()
+        response = query_engine.query(query)
+        print(f"Response: {response}")
+    return index
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process and query PDF files.")
+
+    parser.add_argument("--customer", default="RandstadDigital", help="Customer name")
+    parser.add_argument("--pdf_dir", default="./data", help="Directory containing PDFs")
+    parser.add_argument(
+        "--query",
+        default="What is CX0 customer exprience office?",
+        help="Query to execute",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,52 @@
+import os
+from pathlib import Path
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
-         
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+import data_embedding
+
+st.set_page_config(
+    page_title="Chat with the Randstad Digital docs.",
+    page_icon="💬",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+openai.api_key = os.environ["OPENAI_API_KEY"]
+st.title("Chat with the Randstad Digital docs.")
+
+
+if "messages" not in st.session_state.keys():
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {
+            "role": "assistant",
+            "content": "Ask me a question about Randstad Digital docs.",
+        }
     ]
+
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+    with st.spinner(
+        text="Loading and indexing the documents – hang tight! This should take 1-2 minutes."
+    ):
+        index = data_embedding.main(data_dir=Path(__file__).resolve().parent / "data", query="what is Ausy?")
         return index
 
+
 index = load_data()
-# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="condense_question", verbose=True
+    )
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+if prompt := st.chat_input(
+    "Your question"
+):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
+for message in st.session_state.messages:  # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
@@ -43,4 +57,13 @@ if st.session_state.messages[-1]["role"] != "assistant":
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+            st.session_state.messages.append(message)  # Add response to message history
+            lines = []
+            for i, doc in enumerate(response.source_nodes, start=1):
+                lines.append(f"### Document Chunk: {i}")
+                lines.append(f"__document name: {doc.metadata['file_name']} ({doc.node_id})__")
+                lines.append(f"{doc.text}")
+                lines.append('')  # for a newline between chunks
+            with st.expander("Source Document"):
+                # Hack to get around st.markdown rendering LaTeX
+                st.markdown('\n'.join(lines), unsafe_allow_html=True)
